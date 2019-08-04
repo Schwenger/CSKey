04f58b754464cf48e4543cd2950fe1a55174c7fa
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2019-08-04 12:47:37 +0200

    Copy both layouts without asking.

--- install.py
+++ install.py
@@ -5,29 +5,34 @@ import platform
 import sys
 import subprocess
 
-# Simple routine for asking a yes/no question.
-prompt = lambda q: input(q).lower().strip()[0] == "y" or prompt(q)
+def check_exists(name):
+  if not os.path.isfile(name):
+    print(f"File {name} not found. Did you move it?")
+    sys.exit()
 
-# Prompt for German Umlauts
-if prompt("Do you need German Umlauts?"):
-  filename = "CSKeyWithUmlauts.keylayout"
-else:
-  filename = "CSKey.keylayout"
+layout_with_um = "CSKeyWithUmlauts.keylayout"
+layout = "CSKey.keylayout"
+target = '/Library/Keyboard\ Layouts/'
+cp_layout = f"sudo cp {layout} {target}"
+cp_layout_with_um = f"sudo cp {layout_with_um} {target}"
 
-if not os.path.isfile(filename):
-  print(f"File {filename} not found. Did you move it?")
-  sys.exit()
+commands = [cp_layout, cp_layout_with_um]
 
-if platform.system() == "Darwin":  # check if Mac
-  target = '/Library/Keyboard\\ Layouts/'
-  command = f"sudo cp {filename} {target}"
+check_exists(layout)
+check_exists(layout_with_um)
+
+if platform.system() == "Darwin":   # Check if system is Mac.
+  if not os.path.isdir(target.replace('\\', '')):  # Check if target directory is there.
+    print("Your Keyboard Layout directory cannot be found. Something is strange with your system...")
+    sys.exit()
   if os.geteuid() != 0:
     print(f"We need to copy the layout file to `{target}`. This requires root privileges, so your OS will ask you for your password.")
     print("If you feel uneasy granting root privileges to some dude on the Internet, feel free to copy the file yourself:")
-    print(f"Simply run: `sudo cp {filename} {target}`.")
-    print("After copying, you will need to open Apple > System Preferences > Keyboard > Input Sources, click the little \"+\", go to \"others\" and select the CSKeyboardLayout. Voilà!")
+    print(f"Simply run: `{';'.join(commands)}`.")
+    print("After copying, you will need to open Apple > System Preferences > Keyboard > Input Sources, click the little \"+\", go to \"others\" and select the CSKey layout with or without Umlauts. Voilà!")
     print("Note: if there is already an open System Preferences window, you need to close and re-open it.")
-    os.system(command)
+    for cmd in commands:
+      os.system(cmd)
 else: 
   print("Sorry, We currently only support Mac. You need to install the layout manually.")
 
